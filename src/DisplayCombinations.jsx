dde6ba425349182f8bb802eb8422af84c69f5751
Jeremiah <jeremiahjacobson10@gmail.com>
2024-05-03 18:12:43 -0400

    Update css to support mobile devices

--- src/DisplayCombinations.jsx
+++ src/DisplayCombinations.jsx
@@ -1,7 +1,7 @@
 import CancelIcon from "@mui/icons-material/Cancel";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-import { Tooltip } from "@mui/material";
-import React from "react";
+import { Rating, Tooltip } from "@mui/material";
+// import React from "react";
 import tinycolor from "tinycolor2";
 import "./DisplayCombinations.css";
 
@@ -10,6 +10,14 @@ function DisplayCombinations({ colours }) {
     tinycolor.readability(textColor, bgColor).toFixed(2);
   const isReadable = (textColor, bgColor) =>
     tinycolor.isReadable(textColor, bgColor, { level: "AA", size: "small" });
+
+  const getRating = (contrastRatio) => {
+    if (contrastRatio >= 7) return 5; // AAA for normal text
+    if (contrastRatio >= 4.5) return 4; // AA for normal text or AAA for large text
+    if (contrastRatio >= 3) return 3; // AA for large text or for UI components
+    if (contrastRatio >= 2) return 2; // Below standard, but somewhat discernible
+    return 1; // Poor contrast
+  };
 
   return (
     <div className="colourSectionContainer">
@@ -31,7 +39,7 @@ function DisplayCombinations({ colours }) {
               </div>
               <div className="colourMetrics">
                 <div>
-                  Contrast Ratio: {getContrastRatio(textColor, bgColor)}
+                  Contrast: {getContrastRatio(textColor, bgColor)}
                   <Tooltip
                     title={
                       isReadable(textColor, bgColor)
@@ -49,6 +57,19 @@ function DisplayCombinations({ colours }) {
                     )}
                   </Tooltip>
                 </div>
+                <Tooltip
+                  title={`Contrast Ratio: ${getContrastRatio(
+                    textColor,
+                    bgColor
+                  )}`}
+                >
+                  <Rating
+                    name="read-only"
+                    value={getRating(getContrastRatio(textColor, bgColor))}
+                    readOnly
+                    sx={{ fontSize: { xs: "1rem", sm: "1.25rem" } }} // Responsive font size
+                  />
+                </Tooltip>
               </div>
             </div>
           ))
