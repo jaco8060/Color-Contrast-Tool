df7f1897eaebd9da17f9043ab387055854f78d4c
Jeremiah <jeremiahjacobson10@gmail.com>
2024-05-03 01:06:31 -0400

    update app to reflect change

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,47 @@ import { useState } from "react";
 // import tinycolor from "tinycolor2";
 import { ColourInput } from "./ColorInput";
 import { DisplayCombinations } from "./DisplayCombinations";
+
 function App() {
   const [colours, setColours] = useState([]);
 
-  const addColour = (colour) => {
-    if (!colours.includes(colour) && /^#[0-9A-F]{6}$/i.test(colour)) {
-      setColours([...colours, colour]);
+  const addColour = (newColour) => {
+    if (
+      !colours.some((colour) => colour.hex === newColour.hex) &&
+      /^#[0-9A-F]{6}$/i.test(newColour.hex)
+    ) {
+      setColours([
+        ...colours,
+        { hex: newColour.hex, id: new Date().getTime() },
+      ]);
     } else {
       alert("Please select a valid colour that has not been added yet.");
     }
   };
 
+  const updateColour = (id, newHex) => {
+    setColours(
+      colours.map((colour) =>
+        colour.id === id ? { ...colour, hex: newHex } : colour
+      )
+    );
+  };
+
   return (
     <div>
       <h1>Colour Palette Tester</h1>
+      {colours.map((colour) => (
+        <ColourInput
+          key={colour.id}
+          initialColour={colour.hex}
+          onColourChange={(newHex) => updateColour(colour.id, newHex)}
+        />
+      ))}
       <ColourInput addColour={addColour} />
-      {colours.length > 0 && <DisplayCombinations colours={colours} />}
+      {colours.length > 1 && (
+        <DisplayCombinations colours={colours.map((col) => col.hex)} />
+      )}
     </div>
   );
 }
-
 export default App;
