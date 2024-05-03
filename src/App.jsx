f6b6eea3f5fe2512f6aed1bdad3200dc7e2c1bb4
Jeremiah <jeremiahjacobson10@gmail.com>
2024-05-03 13:02:45 -0400

    Add material UI for UI design -light mode & dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import { useState } from "react";
-// import tinycolor from "tinycolor2";
+import { Button, CssBaseline } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useMemo, useState } from "react";
+import "./App.css";
 import { ColourInput } from "./ColourInput";
 import { DisplayCombinations } from "./DisplayCombinations";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("dark"); // Default to dark mode
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: themeMode,
+        },
+      }),
+    [themeMode] // Recalculate the theme only if themeMode changes
+  );
+
+  const toggleTheme = () => {
+    setThemeMode(themeMode === "dark" ? "light" : "dark");
+  };
+
   const [colours, setColours] = useState([]);
 
   const addColour = (newColour) => {
@@ -33,21 +51,28 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Colour Palette Tester</h1>
-      {colours.map((colour) => (
-        <ColourInput
-          key={colour.id}
-          initialColour={colour.hex}
-          onColourChange={(newHex) => updateColour(colour.id, newHex)}
-          onRemove={() => removeColour(colour.id)}
-        />
-      ))}
-      <ColourInput addColour={addColour} />
-      {colours.length > 1 && (
-        <DisplayCombinations colours={colours.map((col) => col.hex)} />
-      )}
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />{" "}
+      {/* Ensures that the background color covers the whole page */}
+      <div className="mainContainer">
+        <h1>Colour Palette Tester</h1>
+        <Button onClick={toggleTheme} variant="contained" color="primary">
+          Switch to {themeMode === "dark" ? "Light" : "Dark"} Mode
+        </Button>
+        {colours.map((colour) => (
+          <ColourInput
+            key={colour.id}
+            initialColour={colour.hex}
+            onColourChange={(newHex) => updateColour(colour.id, newHex)}
+            onRemove={() => removeColour(colour.id)}
+          />
+        ))}
+        <ColourInput addColour={addColour} />
+        {colours.length > 1 && (
+          <DisplayCombinations colours={colours.map((col) => col.hex)} />
+        )}
+      </div>
+    </ThemeProvider>
   );
 }
 
