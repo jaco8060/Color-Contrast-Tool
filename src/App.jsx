9871015de6089daff99545b8967a4337e234de8a
Jeremiah <jeremiahjacobson10@gmail.com>
2024-05-03 00:38:56 -0400

    Update app.jsx to incorporate all the added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import { useState } from "react";
-import "./App.css";
+import ColourInput from "./ColourInput";
+import DisplayCombinations from "./DisplayCombinations";
 
 function App() {
-  return <></>;
+  const [colours, setColours] = useState([]);
+
+  const addColour = (colour) => {
+    // Ensure that the colour is valid and not already in the list
+    if (tinycolor(colour).isValid() && !colours.includes(colour)) {
+      setColours([...colours, colour]);
+    } else {
+      alert("Invalid colour or colour already added.");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Colour Palette Tester</h1>
+      <ColourInput addColour={addColour} />
+      {colours.length > 1 && <DisplayCombinations colours={colours} />}
+    </div>
+  );
 }
 
 export default App;
