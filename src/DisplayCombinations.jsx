9dfaf5efde2ab1d8fa1e12a19a9081a0a830ab59
Jeremiah <jeremiahjacobson10@gmail.com>
2024-05-11 12:47:14 -0400

    de-memoize the display combinations to remove calculation overhead

--- src/DisplayCombinations.jsx
+++ src/DisplayCombinations.jsx
@@ -5,90 +5,93 @@ import { memo, useMemo } from "react";
 import tinycolor from "tinycolor2";
 import "./DisplayCombinations.css";
 
-const DisplayCombinationItem = memo(
-  ({ bgColor, textColor, onToggleCombination, isSelected }) => {
-    const contrastRatio = tinycolor.readability(bgColor, textColor).toFixed(2);
-    const readable = contrastRatio >= 4.5;
+const DisplayCombinationItem = ({
+  bgColor,
+  textColor,
+  onToggleCombination,
+  isSelected,
+}) => {
+  const contrastRatio = tinycolor.readability(bgColor, textColor).toFixed(2);
+  const readable = contrastRatio >= 4.5;
 
-    const getRating = (contrastRatio) => {
-      if (contrastRatio >= 7) return 5;
-      if (contrastRatio >= 4.5) return 4;
-      if (contrastRatio >= 3) return 3;
-      if (contrastRatio >= 2) return 2;
-      return 1;
-    };
-    const handleCheckboxChange = (event) => {
-      onToggleCombination(bgColor, textColor, event.target.checked);
-    };
+  const getRating = (contrastRatio) => {
+    if (contrastRatio >= 7) return 5;
+    if (contrastRatio >= 4.5) return 4;
+    if (contrastRatio >= 3) return 3;
+    if (contrastRatio >= 2) return 2;
+    return 1;
+  };
+  const handleCheckboxChange = (event) => {
+    onToggleCombination(bgColor, textColor, event.target.checked);
+  };
 
-    return (
-      <div className="colourItemContainer">
-        <div className="colourItemWrapper">
-          <Checkbox
-            checked={isSelected}
-            onChange={handleCheckboxChange}
-            color="primary"
-            sx={{
-              "& .MuiSvgIcon-root": { fontSize: { xs: "14px", sm: "2rem" } },
-            }}
-          />
-          <div className="colourItem">
-            <div
-              className="colourSection"
-              style={{ backgroundColor: bgColor, color: textColor }}
-            >
-              <h1 className="colourSectionText">
-                Background Color: {bgColor} with Text Color: {textColor}
-              </h1>
-            </div>
+  return (
+    <div className="colourItemContainer">
+      <div className="colourItemWrapper">
+        <Checkbox
+          checked={isSelected}
+          onChange={handleCheckboxChange}
+          color="primary"
+          sx={{
+            "& .MuiSvgIcon-root": { fontSize: { xs: "14px", sm: "2rem" } },
+          }}
+        />
+        <div className="colourItem">
+          <div
+            className="colourSection"
+            style={{ backgroundColor: bgColor, color: textColor }}
+          >
+            <h1 className="colourSectionText">
+              Background Color: {bgColor} with Text Color: {textColor}
+            </h1>
           </div>
-        </div>
-        <div className="colourMetrics">
-          <div>
-            Contrast: {contrastRatio}
-            <Tooltip title={readable ? "Readable" : "Not Readable"}>
-              {readable ? (
-                <CheckCircleIcon
-                  className="readableIcon"
-                  color="success"
-                  sx={{
-                    fontSize: {
-                      xs: "0.8rem",
-                      sm: "1.3rem",
-                      md: "1.5rem",
-                      lg: "1.5rem",
-                    },
-                  }}
-                />
-              ) : (
-                <CancelIcon
-                  className="notReadableIcon"
-                  color="error"
-                  sx={{
-                    fontSize: {
-                      xs: "0.8rem",
-                      sm: "1.3rem",
-                      md: "1.5rem",
-                      lg: "1.5rem",
-                    },
-                  }}
-                />
-              )}
-            </Tooltip>
-          </div>
-          <Tooltip title={`Contrast Ratio: ${contrastRatio}`}>
-            <Rating
-              name="read-only"
-              value={getRating(contrastRatio)}
-              readOnly
-              sx={{ fontSize: { xs: "0.8rem", sm: "1.25rem" } }}
-            />
-          </Tooltip>
         </div>
       </div>
-    );
-  }
-);
+      <div className="colourMetrics">
+        <div>
+          Contrast: {contrastRatio}
+          <Tooltip title={readable ? "Readable" : "Not Readable"}>
+            {readable ? (
+              <CheckCircleIcon
+                className="readableIcon"
+                color="success"
+                sx={{
+                  fontSize: {
+                    xs: "0.8rem",
+                    sm: "1.3rem",
+                    md: "1.5rem",
+                    lg: "1.5rem",
+                  },
+                }}
+              />
+            ) : (
+              <CancelIcon
+                className="notReadableIcon"
+                color="error"
+                sx={{
+                  fontSize: {
+                    xs: "0.8rem",
+                    sm: "1.3rem",
+                    md: "1.5rem",
+                    lg: "1.5rem",
+                  },
+                }}
+              />
+            )}
+          </Tooltip>
+        </div>
+        <Tooltip title={`Contrast Ratio: ${contrastRatio}`}>
+          <Rating
+            name="read-only"
+            value={getRating(contrastRatio)}
+            readOnly
+            sx={{ fontSize: { xs: "0.8rem", sm: "1.25rem" } }}
+          />
+        </Tooltip>
+      </div>
+    </div>
+  );
+};
 DisplayCombinationItem.displayName = "DisplayCombinationItem"; // Set display name
 
 function DisplayCombinations({
@@ -97,22 +100,19 @@ function DisplayCombinations({
   selectedCombinations,
   showOnlyReadable,
 }) {
-  const filteredCombinations = useMemo(() => {
-    return colours
-      .flatMap((bgColor) =>
-        colours
-          .filter((textColor) => textColor !== bgColor)
-          .map((textColor) => ({
-            bgColor,
-            textColor,
-            isSelected: selectedCombinations.includes(
-              `${bgColor}-${textColor}`
-            ),
-            readability: tinycolor.readability(bgColor, textColor).toFixed(2),
-          }))
-      )
-      .filter((comb) => !showOnlyReadable || comb.readability >= 4.5);
-  }, [colours, selectedCombinations, showOnlyReadable]);
+  const filteredCombinations = colours
+    .flatMap((bgColor) =>
+      colours
+        .filter((textColor) => textColor !== bgColor)
+        .map((textColor) => ({
+          bgColor,
+          textColor,
+          isSelected: selectedCombinations.includes(`${bgColor}-${textColor}`),
+          readability: tinycolor.readability(bgColor, textColor).toFixed(2),
+        }))
+    )
+    .filter((comb) => !showOnlyReadable || comb.readability >= 4.5);
+
   return (
     <div className="colourSectionContainer">
       {filteredCombinations.map((item, index) => (
